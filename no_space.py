2b6913c20f734b80fab8dc8eee309cb1fc4b7a1f
sam07307176 <162352358+sam07307176@users.noreply.github.com>
2024-04-28 17:24:16 +0800

    我把空格刪掉了，並把打錯的檔案刪除

--- no_space.py
+++
@@ -1,8 +0,0 @@
-import re
-
-with open("./ke_neng_raw.txt", mode='r', encoding="utf-8") as f:
-    dataLIST = f.readlines()
-
-nospace_data = re.sub(r'\n', "", "".join(dataLIST))
-
-print(nospace_data)
